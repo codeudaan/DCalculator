d30f64c192373378961b4f2293196d8f3caf2118
CodeUdaan <social.codeudaan@gmail.com>
2026-07-18 09:17:36 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,9 @@
+import dcalculator as dc
+
+print(dc.add(10, 20))
+print(dc.subtract(20, 10))
+print(dc.multiply(5, 6))
+print(dc.divide(10, 2))
+print(dc.modulus(10, 3))
+print(dc.floor_divide(10, 3))
+print(dc.power(2, 5))
